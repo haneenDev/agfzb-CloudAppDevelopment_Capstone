00220d6d7c23045b828c2feb519cc206029ef9ca
haneenDev <haneen-97@outlook.com>
2022-10-09 10:06:21 +0300

    update

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from . import views
@@ -8,21 +8,14 @@ urlpatterns = [
     # route is a string contains a URL pattern
     # view refers to the view function
     # name the URL
-
+    # path(route='', view=views.index, name='index'),
     # path for about view
-
+    path(route='about', view=views.about, name='about'),
     # path for contact us view
 
-    # path for registration
-
-    # path for login
-
-    # path for logout
-
+    path(route='contact', view=views.contact, name='contact'),
+    
     path(route='', view=views.get_dealerships, name='index'),
 
-    # path for dealer reviews view
-
-    # path for add a review view
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
